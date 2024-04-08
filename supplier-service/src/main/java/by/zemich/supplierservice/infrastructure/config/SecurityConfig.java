07fcd5abfdd3999a32a3d1a93bf69112e7b817e8
Eugene <compserv@yahoo.com>
2024-04-08 19:13:08 +0200

    try to push

--- supplier-service/src/main/java/by/zemich/supplierservice/infrastructure/config/SecurityConfig.java
+++ supplier-service/src/main/java/by/zemich/supplierservice/infrastructure/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/supplier/**").hasAuthority("add_supplier")
                                 .anyRequest().authenticated()
                 )
+                // some comment
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .csrf(CsrfConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
