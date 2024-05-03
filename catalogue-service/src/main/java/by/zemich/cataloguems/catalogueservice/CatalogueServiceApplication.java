515aec6f865acea62be003b044b602d6e71f5bf0
Eugene <compserv@yahoo.com>
2024-05-04 02:07:46 +0300

    'latest'

--- catalogue-service/src/main/java/by/zemich/cataloguems/catalogueservice/CatalogueServiceApplication.java
+++ catalogue-service/src/main/java/by/zemich/cataloguems/catalogueservice/CatalogueServiceApplication.java
@@ -2,8 +2,11 @@ package by.zemich.cataloguems.catalogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableBinding
 public class CatalogueServiceApplication {
 
 	public static void main(String[] args) {
